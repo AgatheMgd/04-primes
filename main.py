6ed760d291dbaf0c7b9f63ada33a8ab838aed19a
agathemaugendre <agathe.maugendre@edu.esiee.fr>
2024-11-07 10:13:17 +0000

    Exercice calcul valeur de vérité nombre premier : 04-primes Agathe Maugendre E2G7

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
+"""
+Fonctions pour calculer les nombres premiers
+"""
+
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """
+    Vérifie si un nombre est premier.
 
-    # votre code ici
+    :param p: Le nombre à vérifier.
+    :return: True si le nombre est premier, False.
 
-    pass
+    >>> isprime(-1)
+    'False'
+    >>> isprime(0)
+    'False'
+    >>> isprime(1)
+    False
+    >>> isprime(2)
+    True
+    >>> isprime(5)
+    True
+    >>> fact(10)
+    False
+    """
+    if p == 1:
+        return False
+    end = int(sqrt(p)) + 1
+    for i in range(2, end):
+        if p % i == 0 :
+            return False
+    return True
 
 #### Fonction principale
 
-
 def main():
-
-    # vos appels à la fonction secondaire ici
+    """ Test le fonctionnement de isprime
+    """
+    isprime(13)
+    isprime(1)
+    isprime(563)
 
     for n in range(100):
         if isprime(n):
